c03b7c45e3b8a7b70b00b81530742f3d17e255f4
Andrew Mason <andrewmcodes@protonmail.com>
2019-11-14 06:23:24 -0500

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,16 +3,16 @@
 set -e
 
 cd "$GITHUB_WORKSPACE" || exit
-[ -z "$GITHUB_TOKEN" ] && echo "Empty" || echo "Not empty"
-export REVIEWDOG_GITHUB_API_TOKEN="$GITHUB_TOKEN"
+
+export REVIEWDOG_GITHUB_API_TOKEN="{$GITHUB_TOKEN}"
 
 if [ "${INPUT_BUNDLE}" != 'true' ]; then [ "${INPUT_BUNDLE}" = 'false' ]; fi
 
 if [ "${INPUT_BUNDLE}" = 'true' ]; then
   bundle install && \
   bundle exec rubocop -v && \
-  bundle exec rubocop | reviewdog -f=rubocop -name="rubocop" -reporter=github-pr-review
+  bundle exec rubocop . | reviewdog -f=rubocop -name="rubocop" -reporter=github-pr-review
 else
   gem install rubocop rubocop-performance rubocop-rails rubocop-minitest rubocop-rspec && \
-  rubocop | reviewdog -f=rubocop -name="rubocop" -reporter=github-pr-review
+  rubocop . | reviewdog -f=rubocop -name="rubocop" -reporter=github-pr-review
 fi
