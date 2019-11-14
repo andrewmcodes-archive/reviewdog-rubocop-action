794a34cad403cba54af251995538c64fdb978f27
Andrew Mason <andrewmcodes@protonmail.com>
2019-11-14 04:51:38 -0500

    sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,12 +16,12 @@ else
   gem install rubocop
   if [ -n "${INPUT_RUBOCOP_PLUGINS}" ];
   then
-    OIFS="{$IFS}";
-    IFS=', ' plugin_array="{$INPUT_RUBOCOP_PLUGINS}"
-    IFS="{$OIFS}";
-    for i in "${plugin_array[@]}"
+    OIFS="${IFS}";
+    IFS=', ' plugin_array="${INPUT_RUBOCOP_PLUGINS}"
+    IFS="${OIFS}";
+    for i in $plugin_array
     do
-      gem install "$i"
+      gem install $i
     done
   fi
   rubocop | reviewdog -f=rubocop -name="rubocop" -reporter=github-pr-review
