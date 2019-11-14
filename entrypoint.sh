c1cd5d672907e4a367441208e6e0c10f886be05a
Andrew Mason <andrewmcodes@protonmail.com>
2019-11-14 06:45:37 -0500

    small tweak

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,8 +9,8 @@ if [ "${INPUT_BUNDLE}" != 'true' ]; then [ "${INPUT_BUNDLE}" = 'false' ]; fi
 if [ "${INPUT_BUNDLE}" = 'true' ]; then
   bundle install && \
   bundle exec rubocop -v && \
-  bundle exec rubocop . | /usr/local/bin/reviewdog -f=rubocop -name="rubocop" -reporter=github-pr-review
+  bundle exec rubocop . | reviewdog -f=rubocop -reporter=github-pr-review
 else
   gem install rubocop rubocop-performance rubocop-rails rubocop-minitest rubocop-rspec && \
-  rubocop . | /usr/local/bin/reviewdog -f=rubocop -name="rubocop" -reporter=github-pr-review
+  rubocop . | reviewdog -f=rubocop -reporter=github-pr-review
 fi
